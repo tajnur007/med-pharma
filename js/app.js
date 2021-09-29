8e6cb061862f7df254012653da8614d8dcfb613a
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-09-29 16:04:15 +0600

    Nav items link added and active page indicator system added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,51 @@
 
+// Active Page Checking 
+let path = location.pathname;
+switch (path) {
+    case '/':
+    case '/index.html':
+        document.getElementById('home').classList.add('active');
+        break;
+    case '/covid-19.html':
+        document.getElementById('covid-19').classList.add('active');
+        document.getElementById('category').classList.add('active');
+        break;
+    case '/allopathic.html':
+        document.getElementById('allopathic').classList.add('active');
+        document.getElementById('category').classList.add('active');
+        break;
+    case '/herbal.html':
+        document.getElementById('herbal').classList.add('active');
+        document.getElementById('category').classList.add('active');
+        break;
+    case '/homeo.html':
+        document.getElementById('homeo').classList.add('active');
+        document.getElementById('category').classList.add('active');
+        break;
+    case '/unani.html':
+        document.getElementById('unani').classList.add('active');
+        document.getElementById('category').classList.add('active');
+        break;
+    case '/vaccine.html':
+        document.getElementById('vaccine').classList.add('active');
+        document.getElementById('category').classList.add('active');
+        break;
+    case '/devices.html':
+        document.getElementById('devices').classList.add('active');
+        document.getElementById('category').classList.add('active');
+        break;
+    case '/health.html':
+        document.getElementById('health').classList.add('active');
+        document.getElementById('category').classList.add('active');
+        break;
+    case '/prescription.html':
+        document.getElementById('prescription').classList.add('active');
+        break;
+    case '/about.html':
+        document.getElementById('about').classList.add('active');
+        break;
+}
+
 // Toggle Sidebar System 
 let checkToggle = false;
 
